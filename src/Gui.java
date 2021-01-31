409538cc7c2eac47b28f6b2c718d89405d88a011
“DawidSkra007” <dawid.skraba@ucdconnect.ie>
2021-01-31 16:24:26 +0000

    Input textfield added that stores values entered

--- src/Gui.java
+++ src/Gui.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Gui {
 
@@ -11,17 +13,35 @@ public class Gui {
 
     //implementing output terminal
 
-    JPanel input = new JPanel();
+    public Component text() {
+        JPanel input = new JPanel();
+        JTextField textField = new JTextField("",83);
 
-    //implementing input box
+        Action action = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String in = textField.getText();
+                textField.setText("");
+                System.out.println(in);//prints to console (debugging)
+            }
+        };
+
+        textField.addActionListener(action);
+        String value = textField.getText();
+        input.add(textField);
+        return input;
+    }
 
 
     public static void main(String[] args) {
+        Gui gui = new Gui();
+
         JFrame jfr = new JFrame("Risk");
         jfr.setSize(1000, 600);
-        jfr.setLayout(new FlowLayout());
+        jfr.setLayout(new BorderLayout());
         jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfr.add(gui.text(),BorderLayout.SOUTH);
+        jfr.setResizable(false);
         jfr.setVisible(true);
-
     }
 }
