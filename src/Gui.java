e5c64ea38c1b1953e7a4e209a3187fe561917972
AdiG96 <aditya.marthandagatt@gmail.com>
2021-02-06 12:05:54 +0530

    player names work

--- src/Gui.java
+++ src/Gui.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
+
 class Player1{
     public static String name;
 }
@@ -11,22 +12,26 @@ class Player2{
 }
 
 public class Gui {
-    public JTextArea textArea;
+    public static String lastCommand;
+    public static int count = 0;
     private  static final JTextArea OutputArea = new JTextArea(5,10);
 
     //map
 
 
-    public static JScrollPane Output(){ //output
+
+
+    public JScrollPane Output(){ //output
         JPanel OutputPanel = new JPanel();
 
         OutputPanel.setBackground(Color.LIGHT_GRAY);
         JScrollPane scrollPane = new JScrollPane(OutputArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         OutputPanel.add(scrollPane,BorderLayout.CENTER);
         OutputArea.setEditable(false);
         OutputPanel.add(scrollPane);
         OutputPanel.setVisible(true);
+        
         return scrollPane;
     }
     public static void addText(String s){
@@ -34,26 +39,41 @@ public class Gui {
     }
 
     public Component text() {
-        JPanel input = new JPanel();
         JTextField textField = new JTextField("",83);
+        JPanel input = new JPanel();
+
+
 
         Action action = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Player1.name = textField.getText();
-                textArea.append(Player1.name + "\n");
+                addText(textField.getText());
+                lastCommand = textField.getText();
+                count++;
                 textField.setText("");
 
-                Player2.name = textField.getText();
-                textArea.append(Player2.name + "\n");
-                textField.setText("");
+                if(count == 1){
+                    Player1.name = lastCommand;
+                    addText("Enter name of player 2");
+                }
+                else if(count == 2){
+                    Player2.name = lastCommand;
+                }
+
             }
         };
 
         textField.addActionListener(action);
-        String value = textField.getText();
         input.add(textField);
+
+
         return input;
+    }
+
+
+
+    public static void showNames(){
+        addText(Player1.name + " " + Player2.name);
     }
 
     public static void main(String[] args) {
@@ -68,5 +88,11 @@ public class Gui {
         jfr.add(gui.Output(),BorderLayout.WEST);
         jfr.setResizable(false);
         jfr.setVisible(true);
+
+
+        addText("Enter name of player 1");
+
+
+
     }
 }
